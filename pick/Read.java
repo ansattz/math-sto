80f4e9ad2019d24f41325f80e3d897bdedbcfb4d
ansattz <dvieiras@aol.com>
2022-09-15 17:10:56 -0300

    abrir os pdfs; ainda em testes; GUI precisa ser melhorada

---
+++ pick/Read.java
@@ -0,0 +1,39 @@
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.*;
+
+public class Read {
+
+   public static void main(String[] args) {
+      while (true) {
+         String op = JOptionPane.showInputDialog(null, "Escolha um pdf",
+               "Notas em Matematica", JOptionPane.PLAIN_MESSAGE, null,
+               new Object[] {
+                     "Algebra",
+                     "Teoria dos Conjuntos" },
+               "PDFs").toString();
+         switch (op) {
+            case "Algebra":
+               if (Desktop.isDesktopSupported()) {
+                  try {
+                     File algebra = new File("../algebra/main.pdf");
+                     Desktop.getDesktop().open(algebra);
+                  } catch (IOException ex) {
+                  }
+               }
+               break;
+            case "Teoria dos Conjuntos":
+               if (Desktop.isDesktopSupported()) {
+                  try {
+                     File stheory = new File("../set_theory/main.pdf");
+                     Desktop.getDesktop().open(stheory);
+                  } catch (IOException ex) {
+                  }
+               }
+               break;
+         }
+      }
+   }
+}
